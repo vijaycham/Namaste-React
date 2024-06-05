879796f0f77a6b4bb209da83bfe94f901c2e0d71
vijaycham <104141481+vijaycham@users.noreply.github.com>
2024-06-05 23:36:05 +0530

    Ignited the App

--- App.js
+++ App.js
@@ -15,16 +15,17 @@
 //   { id: "heading" },
 //   "Hello world from React!"
 // );
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement("div", { id: "child1" }, [
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "This is a vijay"),
+    React.createElement("h2", {}, "Learning react from Namaste React"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "This is h1 Tag"),
     React.createElement("h2", {}, "This is h2 Tag"),
-  ]), React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "This is h1 Tag"),
-    React.createElement("h2", {}, "This is h2 Tag"),
-  ])]
-);
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
